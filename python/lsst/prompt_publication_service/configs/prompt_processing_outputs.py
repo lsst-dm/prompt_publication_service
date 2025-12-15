4a18e4e1dc32b479ca3fea5c7ff9f1474eebeb7c
David H. Irving <david.irving@noirlab.edu>
2025-12-15 15:46:43 -0700

    fixup config

--- python/lsst/prompt_publication_service/configs/prompt_processing_outputs.py
+++ python/lsst/prompt_publication_service/configs/prompt_processing_outputs.py
@@ -21,33 +21,33 @@
 
 from ..config import DatasetTypeConfiguration, DatasetTypeConfigurationItem
 
-EMBARGO_PERIOD_HOURS = 80
-TIER2_RETENTION_PERIOD_DAYS = 30
+_EMBARGO_PERIOD_HOURS = 80
+_TIER2_RETENTION_PERIOD_DAYS = 30
 
-PROVENANCE = DatasetTypeConfigurationItem(
+_PROVENANCE = DatasetTypeConfigurationItem(
     embargo_period_hours=0, publish_to_public=True, retention_period_days="forever"
 )
 
-TIER1_PIXEL = DatasetTypeConfigurationItem(
-    embargo_period_hours=EMBARGO_PERIOD_HOURS, publish_to_public=True, retention_period_days="forever"
+_TIER1_PIXEL = DatasetTypeConfigurationItem(
+    embargo_period_hours=_EMBARGO_PERIOD_HOURS, publish_to_public=True, retention_period_days="forever"
 )
 
-TIER1_NONPIXEL = DatasetTypeConfigurationItem(
+_TIER1_NONPIXEL = DatasetTypeConfigurationItem(
     embargo_period_hours=0, publish_to_public=True, retention_period_days="forever"
 )
 
-TIER2_PIXEL = DatasetTypeConfigurationItem(
-    embargo_period_hours=EMBARGO_PERIOD_HOURS,
+_TIER2_PIXEL = DatasetTypeConfigurationItem(
+    embargo_period_hours=_EMBARGO_PERIOD_HOURS,
     publish_to_public=True,
-    retention_period_days=TIER2_RETENTION_PERIOD_DAYS,
+    retention_period_days=_TIER2_RETENTION_PERIOD_DAYS,
 )
 
-TIER2_NONPIXEL = DatasetTypeConfigurationItem(
-    embargo_period_hours=0, publish_to_public=True, retention_period_days=TIER2_RETENTION_PERIOD_DAYS
+_TIER2_NONPIXEL = DatasetTypeConfigurationItem(
+    embargo_period_hours=0, publish_to_public=True, retention_period_days=_TIER2_RETENTION_PERIOD_DAYS
 )
 
-USDF_INTERNAL_NONPIXEL = DatasetTypeConfigurationItem(
-    embargo_period_hours=0, publish_to_public=False, retention_period_days=TIER2_RETENTION_PERIOD_DAYS
+_USDF_INTERNAL_NONPIXEL = DatasetTypeConfigurationItem(
+    embargo_period_hours=0, publish_to_public=False, retention_period_days=_TIER2_RETENTION_PERIOD_DAYS
 )
 
 
@@ -55,105 +55,105 @@ USDF_INTERNAL_NONPIXEL = DatasetTypeConfigurationItem(
 PROMPT_PROCESSING_OUTPUT_CONFIG = DatasetTypeConfiguration(
     {
         # Tier 1
-        "preliminary_visit_image": TIER1_PIXEL,
-        "preliminary_visit_image_background": TIER1_PIXEL,
-        "difference_kernel": TIER1_NONPIXEL,
-        "template_detector": TIER1_PIXEL,
+        "preliminary_visit_image": _TIER1_PIXEL,
+        "preliminary_visit_image_background": _TIER1_PIXEL,
+        "difference_kernel": _TIER1_NONPIXEL,
+        "template_detector": _TIER1_PIXEL,
         # Tier 2
-        "difference_image": TIER2_PIXEL,
-        "single_visit_star_footprints": TIER2_NONPIXEL,
-        "dia_source_apdb": TIER2_NONPIXEL,
-        "dia_forced_source_apdb": TIER2_NONPIXEL,
-        "dia_object_apdb": TIER2_NONPIXEL,
-        "marginal_new_dia_source": TIER2_NONPIXEL,
-        "ss_source_direct_detector": TIER2_NONPIXEL,
-        "ss_object_direct_unassociated": TIER2_NONPIXEL,
-        "ss_object_unassociated_detector": TIER2_NONPIXEL,
-        "ss_source_detector": TIER2_NONPIXEL,
-        "regionTimeInfo": TIER2_NONPIXEL,
+        "difference_image": _TIER2_PIXEL,
+        "single_visit_star_footprints": _TIER2_NONPIXEL,
+        "dia_source_apdb": _TIER2_NONPIXEL,
+        "dia_forced_source_apdb": _TIER2_NONPIXEL,
+        "dia_object_apdb": _TIER2_NONPIXEL,
+        "marginal_new_dia_source": _TIER2_NONPIXEL,
+        "ss_source_direct_detector": _TIER2_NONPIXEL,
+        "ss_object_direct_unassociated": _TIER2_NONPIXEL,
+        "ss_object_unassociated_detector": _TIER2_NONPIXEL,
+        "ss_source_detector": _TIER2_NONPIXEL,
+        "regionTimeInfo": _TIER2_NONPIXEL,
         # Non-public
-        "dia_source_detector": USDF_INTERNAL_NONPIXEL,
-        "dia_source_schema": USDF_INTERNAL_NONPIXEL,
-        "dia_source_unfiltered": USDF_INTERNAL_NONPIXEL,
-        "new_dia_source": USDF_INTERNAL_NONPIXEL,
-        "preloaded_dia_forced_source": USDF_INTERNAL_NONPIXEL,
-        "preloaded_dia_object": USDF_INTERNAL_NONPIXEL,
-        "preloaded_dia_source": USDF_INTERNAL_NONPIXEL,
-        "preloaded_ss_object": USDF_INTERNAL_NONPIXEL,
-        "single_visit_star_schema": USDF_INTERNAL_NONPIXEL,
+        "dia_source_detector": _USDF_INTERNAL_NONPIXEL,
+        "dia_source_schema": _USDF_INTERNAL_NONPIXEL,
+        "dia_source_unfiltered": _USDF_INTERNAL_NONPIXEL,
+        "new_dia_source": _USDF_INTERNAL_NONPIXEL,
+        "preloaded_dia_forced_source": _USDF_INTERNAL_NONPIXEL,
+        "preloaded_dia_object": _USDF_INTERNAL_NONPIXEL,
+        "preloaded_dia_source": _USDF_INTERNAL_NONPIXEL,
+        "preloaded_ss_object": _USDF_INTERNAL_NONPIXEL,
+        "single_visit_star_schema": _USDF_INTERNAL_NONPIXEL,
         # Miscellaneous provenance datasets (config/log/metadata/packages).
-        "analyzeAssociateDiaSourceTiming_config": PROVENANCE,
-        "analyzeAssociateDiaSourceTiming_log": PROVENANCE,
-        "analyzeAssociateDiaSourceTiming_metadata": PROVENANCE,
-        "analyzeAssociatedDiaSourceTable_config": PROVENANCE,
-        "analyzeAssociatedDiaSourceTable_log": PROVENANCE,
-        "analyzeAssociatedDiaSourceTable_metadata": PROVENANCE,
-        "analyzeAssociatedDirectSolarSystemObjectTable_config": PROVENANCE,
-        "analyzeAssociatedDirectSolarSystemObjectTable_log": PROVENANCE,
-        "analyzeAssociatedDirectSolarSystemObjectTable_metadata": PROVENANCE,
-        "analyzeDiaSourceAssociationMetrics_config": PROVENANCE,
-        "analyzeDiaSourceAssociationMetrics_log": PROVENANCE,
-        "analyzeDiaSourceAssociationMetrics_metadata": PROVENANCE,
-        "analyzeDiaSourceDetectionMetrics_config": PROVENANCE,
-        "analyzeDiaSourceDetectionMetrics_log": PROVENANCE,
-        "analyzeDiaSourceDetectionMetrics_metadata": PROVENANCE,
-        "analyzeImageDifferenceMetrics_config": PROVENANCE,
-        "analyzeImageDifferenceMetrics_log": PROVENANCE,
-        "analyzeImageDifferenceMetrics_metadata": PROVENANCE,
-        "analyzeLoadDiaCatalogsMetrics_config": PROVENANCE,
-        "analyzeLoadDiaCatalogsMetrics_log": PROVENANCE,
-        "analyzeLoadDiaCatalogsMetrics_metadata": PROVENANCE,
-        "analyzePreliminarySummaryStats_config": PROVENANCE,
-        "analyzePreliminarySummaryStats_log": PROVENANCE,
-        "analyzePreliminarySummaryStats_metadata": PROVENANCE,
-        "analyzeTrailedDiaSourceTable_config": PROVENANCE,
-        "analyzeTrailedDiaSourceTable_log": PROVENANCE,
-        "analyzeTrailedDiaSourceTable_metadata": PROVENANCE,
-        "analyzeUnassociatedDirectSolarSystemObjectTable_config": PROVENANCE,
-        "analyzeUnassociatedDirectSolarSystemObjectTable_log": PROVENANCE,
-        "analyzeUnassociatedDirectSolarSystemObjectTable_metadata": PROVENANCE,
-        "associateApdb_config": PROVENANCE,
-        "associateApdb_log": PROVENANCE,
-        "associateApdb_metadata": PROVENANCE,
-        "associateSolarSystemDirectSource_config": PROVENANCE,
-        "associateSolarSystemDirectSource_log": PROVENANCE,
-        "associateSolarSystemDirectSource_metadata": PROVENANCE,
-        "buildTemplate_config": PROVENANCE,
-        "buildTemplate_log": PROVENANCE,
-        "buildTemplate_metadata": PROVENANCE,
-        "calibrateImage_config": PROVENANCE,
-        "calibrateImage_log": PROVENANCE,
-        "calibrateImage_metadata": PROVENANCE,
-        "computeReliability_config": PROVENANCE,
-        "computeReliability_log": PROVENANCE,
-        "computeReliability_metadata": PROVENANCE,
-        "detectAndMeasureDiaSource_config": PROVENANCE,
-        "detectAndMeasureDiaSource_log": PROVENANCE,
-        "detectAndMeasureDiaSource_metadata": PROVENANCE,
-        "filterDiaSourcePostReliability_config": PROVENANCE,
-        "filterDiaSourcePostReliability_log": PROVENANCE,
-        "filterDiaSourcePostReliability_metadata": PROVENANCE,
-        "filterDiaSource_config": PROVENANCE,
-        "filterDiaSource_log": PROVENANCE,
-        "filterDiaSource_metadata": PROVENANCE,
-        "isr_config": PROVENANCE,
-        "isr_log": PROVENANCE,
-        "isr_metadata": PROVENANCE,
-        "loadDiaCatalogs_config": PROVENANCE,
-        "loadDiaCatalogs_log": PROVENANCE,
-        "loadDiaCatalogs_metadata": PROVENANCE,
-        "mpSkyEphemerisQuery_config": PROVENANCE,
-        "mpSkyEphemerisQuery_log": PROVENANCE,
-        "mpSkyEphemerisQuery_metadata": PROVENANCE,
-        "packages": PROVENANCE,
-        "singleFrameDetectAndMeasure_config": PROVENANCE,
-        "singleFrameDetectAndMeasure_log": PROVENANCE,
-        "singleFrameDetectAndMeasure_metadata": PROVENANCE,
-        "standardizeDiaSource_config": PROVENANCE,
-        "standardizeDiaSource_log": PROVENANCE,
-        "standardizeDiaSource_metadata": PROVENANCE,
-        "subtractImages_config": PROVENANCE,
-        "subtractImages_log": PROVENANCE,
-        "subtractImages_metadata": PROVENANCE,
+        "analyzeAssociateDiaSourceTiming_config": _PROVENANCE,
+        "analyzeAssociateDiaSourceTiming_log": _PROVENANCE,
+        "analyzeAssociateDiaSourceTiming_metadata": _PROVENANCE,
+        "analyzeAssociatedDiaSourceTable_config": _PROVENANCE,
+        "analyzeAssociatedDiaSourceTable_log": _PROVENANCE,
+        "analyzeAssociatedDiaSourceTable_metadata": _PROVENANCE,
+        "analyzeAssociatedDirectSolarSystemObjectTable_config": _PROVENANCE,
+        "analyzeAssociatedDirectSolarSystemObjectTable_log": _PROVENANCE,
+        "analyzeAssociatedDirectSolarSystemObjectTable_metadata": _PROVENANCE,
+        "analyzeDiaSourceAssociationMetrics_config": _PROVENANCE,
+        "analyzeDiaSourceAssociationMetrics_log": _PROVENANCE,
+        "analyzeDiaSourceAssociationMetrics_metadata": _PROVENANCE,
+        "analyzeDiaSourceDetectionMetrics_config": _PROVENANCE,
+        "analyzeDiaSourceDetectionMetrics_log": _PROVENANCE,
+        "analyzeDiaSourceDetectionMetrics_metadata": _PROVENANCE,
+        "analyzeImageDifferenceMetrics_config": _PROVENANCE,
+        "analyzeImageDifferenceMetrics_log": _PROVENANCE,
+        "analyzeImageDifferenceMetrics_metadata": _PROVENANCE,
+        "analyzeLoadDiaCatalogsMetrics_config": _PROVENANCE,
+        "analyzeLoadDiaCatalogsMetrics_log": _PROVENANCE,
+        "analyzeLoadDiaCatalogsMetrics_metadata": _PROVENANCE,
+        "analyzePreliminarySummaryStats_config": _PROVENANCE,
+        "analyzePreliminarySummaryStats_log": _PROVENANCE,
+        "analyzePreliminarySummaryStats_metadata": _PROVENANCE,
+        "analyzeTrailedDiaSourceTable_config": _PROVENANCE,
+        "analyzeTrailedDiaSourceTable_log": _PROVENANCE,
+        "analyzeTrailedDiaSourceTable_metadata": _PROVENANCE,
+        "analyzeUnassociatedDirectSolarSystemObjectTable_config": _PROVENANCE,
+        "analyzeUnassociatedDirectSolarSystemObjectTable_log": _PROVENANCE,
+        "analyzeUnassociatedDirectSolarSystemObjectTable_metadata": _PROVENANCE,
+        "associateApdb_config": _PROVENANCE,
+        "associateApdb_log": _PROVENANCE,
+        "associateApdb_metadata": _PROVENANCE,
+        "associateSolarSystemDirectSource_config": _PROVENANCE,
+        "associateSolarSystemDirectSource_log": _PROVENANCE,
+        "associateSolarSystemDirectSource_metadata": _PROVENANCE,
+        "buildTemplate_config": _PROVENANCE,
+        "buildTemplate_log": _PROVENANCE,
+        "buildTemplate_metadata": _PROVENANCE,
+        "calibrateImage_config": _PROVENANCE,
+        "calibrateImage_log": _PROVENANCE,
+        "calibrateImage_metadata": _PROVENANCE,
+        "computeReliability_config": _PROVENANCE,
+        "computeReliability_log": _PROVENANCE,
+        "computeReliability_metadata": _PROVENANCE,
+        "detectAndMeasureDiaSource_config": _PROVENANCE,
+        "detectAndMeasureDiaSource_log": _PROVENANCE,
+        "detectAndMeasureDiaSource_metadata": _PROVENANCE,
+        "filterDiaSourcePostReliability_config": _PROVENANCE,
+        "filterDiaSourcePostReliability_log": _PROVENANCE,
+        "filterDiaSourcePostReliability_metadata": _PROVENANCE,
+        "filterDiaSource_config": _PROVENANCE,
+        "filterDiaSource_log": _PROVENANCE,
+        "filterDiaSource_metadata": _PROVENANCE,
+        "isr_config": _PROVENANCE,
+        "isr_log": _PROVENANCE,
+        "isr_metadata": _PROVENANCE,
+        "loadDiaCatalogs_config": _PROVENANCE,
+        "loadDiaCatalogs_log": _PROVENANCE,
+        "loadDiaCatalogs_metadata": _PROVENANCE,
+        "mpSkyEphemerisQuery_config": _PROVENANCE,
+        "mpSkyEphemerisQuery_log": _PROVENANCE,
+        "mpSkyEphemerisQuery_metadata": _PROVENANCE,
+        "packages": _PROVENANCE,
+        "singleFrameDetectAndMeasure_config": _PROVENANCE,
+        "singleFrameDetectAndMeasure_log": _PROVENANCE,
+        "singleFrameDetectAndMeasure_metadata": _PROVENANCE,
+        "standardizeDiaSource_config": _PROVENANCE,
+        "standardizeDiaSource_log": _PROVENANCE,
+        "standardizeDiaSource_metadata": _PROVENANCE,
+        "subtractImages_config": _PROVENANCE,
+        "subtractImages_log": _PROVENANCE,
+        "subtractImages_metadata": _PROVENANCE,
     }
 )
